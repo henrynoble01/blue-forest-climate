a107d4c4eeaa0589b84a88e6a9e4247956d27291
henrynoble01 <35652932+henrynoble01@users.noreply.github.com>
2022-12-17 14:47:08 +0100

    Update upload-widget.tsx

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -28,8 +28,8 @@ const CloudinaryUploadWidget: React.FC<IUploadWidgetProp> = ({ setImgUrl }) => {
           multiple: false, //restrict upload to a single file
           // tags: ["users", "profile"], //add the given tags to the uploaded files
           // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-          clientAllowedFormats: ["images"], //restrict uploading to image files only
-          maxImageFileSize: 2000000, //restrict file size to less than 2MB
+         // clientAllowedFormats: ["images"], //restrict uploading to image files only
+         // maxImageFileSize: 2000000, //restrict file size to less than 2MB
           // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
           // theme: "purple", //change to a purple theme
         },
